7372e460b1ebfd21767dbce8f96d2091972ec1d9
Atish Kumar Sahu <kumarsahuatishoff280301@gmail.com>
2025-04-08 13:32:09 +0530

    Final Commit

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,22 +1,50 @@
+// import BlogOverview from "@/components/blogOverview";
+// async function fetchListOfBlogs () {
+//    try {
+//     const apiResponse =  await fetch('https://next-js-project-02-blog-project.vercel.app/api/getblog',{
+//         method: 'GET',
+//         cache: 'no-store'
+//     });
+//     const result = await apiResponse.json();
+//     return result?.data;
+//    } catch (error) {
+//     throw new Error(error);
+//    } 
+// }
+// export default async function Blogs () {
+//     const blogList = await fetchListOfBlogs();
+//     return (
+//         <>
+//             <BlogOverview blogList={blogList} />
+//         </>
+//     );
+// }
+
+export const dynamic = "force-dynamic";
+
 import BlogOverview from "@/components/blogOverview";
-async function fetchListOfBlogs () {
-   try {
-    const apiResponse =  await fetch('https://next-js-project-02-blog-project.vercel.app/api/getblog',{
-        method: 'GET',
-        cache: 'no-store'
-    });
+
+async function fetchListOfBlogs() {
+  try {
+    const apiResponse = await fetch(
+      "https://next-js-project-02-blog-project.vercel.app/api/getblog",
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
     const result = await apiResponse.json();
     return result?.data;
-   } catch (error) {
+  } catch (error) {
     throw new Error(error);
-   } 
+  }
 }
-export default async function Blogs () {
-    const blogList = await fetchListOfBlogs();
-    return (
-        <>
-            <BlogOverview blogList={blogList} />
-        </>
-    );
+
+export default async function Blogs() {
+  const blogList = await fetchListOfBlogs();
+  return (
+    <>
+      <BlogOverview blogList={blogList} />
+    </>
+  );
 }
-// 
