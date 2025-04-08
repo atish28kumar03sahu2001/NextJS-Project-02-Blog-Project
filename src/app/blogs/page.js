4e16eb2d39ee1b3db2ed0e3d28330397f8de409f
Atish Kumar Sahu <160933920+atish28kumar03sahu2001@users.noreply.github.com>
2025-04-08 13:38:53 +0530

    Final Commit

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,25 +1,3 @@
-// import BlogOverview from "@/components/blogOverview";
-// async function fetchListOfBlogs () {
-//    try {
-//     const apiResponse =  await fetch('https://next-js-project-02-blog-project.vercel.app/api/getblog',{
-//         method: 'GET',
-//         cache: 'no-store'
-//     });
-//     const result = await apiResponse.json();
-//     return result?.data;
-//    } catch (error) {
-//     throw new Error(error);
-//    } 
-// }
-// export default async function Blogs () {
-//     const blogList = await fetchListOfBlogs();
-//     return (
-//         <>
-//             <BlogOverview blogList={blogList} />
-//         </>
-//     );
-// }
-
 export const dynamic = "force-dynamic";
 
 import BlogOverview from "@/components/blogOverview";
